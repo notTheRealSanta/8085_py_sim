94bb9dc7d8d4e20beb4dff53e2369b3d6d006027
NotTheRealSanta <ucsantosh@gmail.com>
2017-10-24 07:22:48 +0530

    updated_final

--- sim.py
+++ sim.py
@@ -16,6 +16,7 @@ memory_values = [""] * hun
 #Just some Functions
 def code_input () :
     
+    print("\nInput 8085 code : \nsyntax: <instruction><space><operand1><comma><space><operand2>, eg:MOV B, A\n(non-case-sensitive)\n\n")
     line = ""
     i = 0
     while line != "HLT ".upper() : #stops sim program when 'hlt' is the input
@@ -40,14 +41,14 @@ def code_input () :
 
         i += 1
 
-    printl(code_commands)
-    printl(code_operand1)
-    printl(code_operand2)
+    # printl(code_commands)
+    # printl(code_operand1)
+    # printl(code_operand2)
 
 
 def input_memory () :
 
-    print("Type esc to stop input!")
+    print("\nInput Memory Location and values. \nsyntax: <location><space><value>, eg:4000 f3\nType 'esc' to stop input!\n\n")
 
     line = ""
     i = 0 
@@ -56,10 +57,22 @@ def input_memory () :
         if line != "esc ".upper() :
             memory_location[i] = line [:line.find(" ")]
             memory_values[i] = line [line.find(" ")+1 : line.find(" ")+3]
+            if memory_values[i] != "":
+                memory_values[i] =  int(memory_values[i], 16) 
         i += 1
     
-    printl(memory_location)
-    printl(memory_values)
+
+    # printl(memory_location)
+    # printl(memory_values)
+    print("\n\n")
+
+def show_memory () :
+
+    print("\nList of all memories used and their values \n\n")
+
+    for i in range(len(memory_location)):
+        if memory_location[i] != "" :
+            print (memory_location[i], " : ", hex(int(memory_values[i])) ," (",memory_values[i],")")
 
 def interpret() :
     
@@ -67,11 +80,25 @@ def interpret() :
     commanding = {
         'LDA' : LDA,
         'LXI' : LXI,
+        'STA' : STA,
+        'MOV' : MOV,
+        'MVI' : MVI,
+        'INX' : INX,
+        'ADD' : ADD
     }
 
     index = 0 # acts like the program counter
     while code_commands[index] != "HLT":
         commanding[code_commands[index]] ( code_operand1[index] ,  code_operand2[index]  )
+        
+        print ("\nInstruction :",code_commands[index])
+        if code_operand1[index] != "" :
+            print ("Operand1 :",code_operand1[index])
+        if code_operand2[index] != "" :
+            print ("Operand2 :",code_operand2[index])
+        
+        print(reg.registers)
+        print("")
         index += 1
     
 #command Fucntions
@@ -82,9 +109,48 @@ def LDA ( address = -1 , op2 = 0 ) :
 
 def LXI ( address = -1 , value = -1 ) :
     
-    if(address != -1) :
-        reg.registers[address] = value
+    if(address != -1 and address != 'H') :
+        reg.registers[address] = int (value, base = 16)
+    if address == 'H':
+        reg.registers[address] = int (value)
 
+def STA (op1 = -1, op2 = 'A') :
+    
+    if(op1 in memory_location) :
+        memory_values[memory_location.index(op1)] = reg.registers[op2]
+
+    else :
+        memory_location.append(op1)
+        memory_values.append(reg.registers[op2])
+
+def MOV (op1 = -1, op2 = -1) :
+    
+    if ( op1 in reg.registers and op2 in reg.registers):
+        reg.registers[op1] = reg.registers[op2]
+
+    if (op2 == 'M') :
+        reg.registers[op1] = int (memory_values[memory_location.index(str(reg.registers['H']))]) 
+
+    if (op1 == 'M') :
+        STA (str(reg.registers['H']) , op2)    
+
+def MVI (op1 = -1, op2 = -1) :
+    
+    if ( op1 in reg.registers ):
+        reg.registers[op1] = op2
+
+def INX (op1 = -1, op2 = -1) :
+
+    if op1 != -1 :
+        reg.registers[op1] = reg.registers[op1] + 1
+
+def ADD (op1 = -1, op2 = -1) :
+    
+    if(op1 == 'M') :
+        reg.registers['A'] += memory_values[memory_location.index(str(reg.registers['H']))]
+
+    if ( op1 in reg.registers ):
+        reg.registers['A'] += reg.registers[op1]
 
 #Main function
 def main() :
@@ -92,7 +158,7 @@ def main() :
     code_input()
     input_memory()
     interpret()
-    print(reg.registers)
+    show_memory()
 
 if __name__ == '__main__':
     main()
